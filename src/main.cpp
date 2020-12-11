7c1cfaeff227efd56e3c1689f8714efc44c9ca87
nacl <uhohnaomi@gmail.com>
2020-12-10 18:14:24 -0800

    Decompose code a bit

--- src/main.cpp
+++ src/main.cpp
@@ -20,88 +20,116 @@
 #include <thread>
 #include <math.h>
 
+struct WindowContext {
+   Display *d;
+   Window root;
+   Window overlay;
+   XVisualInfo vinfo;
+   int screen_w, screen_h;
+};
+
+struct CairoContext {
+   cairo_surface_t* surf;
+   cairo_t* cr;
+};
+
 struct Coordinate {
    int x, y;
 };
 
-int check_event(cairo_surface_t *sfc, int block)
-{
-   char keybuf[8];
-   KeySym key;
-   XEvent e;
+struct Color {
+   double r, g, b, a;
+};
 
-   for (;;)
-   {
-      if (block || XPending(cairo_xlib_surface_get_display(sfc))) {
-         XNextEvent(cairo_xlib_surface_get_display(sfc), &e);
-      } else {
-         return 0;
-      }
-
-      // switch (e.type)
-      // {
-      //    case ButtonPress:
-      //       return -e.xbutton.button;
-      //    case KeyPress:
-      //       XLookupString(&e.xkey, keybuf, sizeof(keybuf), &key, NULL);
-      //       return key;
-      //    default:
-      //       fprintf(stderr, "Dropping unhandled XEevent.type = %d.\n", e.type);
-      //       return 0;
-      // }
-   }
-}
-
-
-void draw(cairo_t *cr, const std::vector<Coordinate> &coords) {
-   int d = 10;
-
+void draw(cairo_t *cr, const std::vector<Coordinate> &coords, double size, const Color &color) {
    cairo_save (cr);
    cairo_set_source_rgba (cr, 0.0, 0.0, 0.0, 0.0);
    cairo_set_operator (cr, CAIRO_OPERATOR_SOURCE);
    cairo_paint (cr);
    cairo_restore (cr);
 
-   cairo_set_source_rgba(cr, 1.0, 0.0, 0.0, 0.75);
+   cairo_set_source_rgba(cr, color.r, color.g, color.b, color.a);
 
-   float size = 2;
-   float max_size = 7;
+   double size_step = size/7.0;
+   double cur_size = size - size_step*5;
    for (Coordinate c : coords) {
-      cairo_arc (cr, c.x, c.y, size, 0., 2 * M_PI);
+      cairo_arc (cr, c.x, c.y, cur_size, 0., 2 * M_PI);
       cairo_fill(cr);
-      if (size < max_size) {
-         size += 1;
+      if (cur_size < size) {
+         cur_size += size_step;
       }
    }
 }
 
-int main() {
-   Display *d;
-   if ((d = XOpenDisplay(NULL)) == NULL) {
+WindowContext initialize_xlib(){
+   WindowContext ctx;
+   if ((ctx.d = XOpenDisplay(NULL)) == NULL) {
       exit(1);
    }
 
    int xi_opcode, xi_event, error;
-   if (!XQueryExtension(d, "XInputExtension", &xi_opcode, &xi_event, &error)) {
+   if (!XQueryExtension(ctx.d, "XInputExtension", &xi_opcode, &xi_event, &error)) {
      fprintf(stderr, "Error: XInput extension is not supported!\n");
-     return 1;
+     exit(1);
    }
 
    int major = 2;
    int minor = 0;
-   int retval = XIQueryVersion(d, &major, &minor);
+   int retval = XIQueryVersion(ctx.d, &major, &minor);
    if (retval != Success) {
      fprintf(stderr, "Error: XInput 2.0 is not supported (ancient X11?)\n");
-     return 1;
+     exit(1);
    }
+   
+   ctx.root = DefaultRootWindow(ctx.d);
 
+   return ctx;
+}
 
-   Window root = DefaultRootWindow(d);
-   int default_screen = XDefaultScreen(d);
+void initialize_window(WindowContext &ctx){
+   int default_screen = XDefaultScreen(ctx.d);
 
-   int screen_w = DisplayWidth(d, default_screen);
-   int screen_h = DisplayHeight(d, default_screen);
+   ctx.screen_w = DisplayWidth(ctx.d, default_screen);
+   ctx.screen_h = DisplayHeight(ctx.d, default_screen);
 
+   XSetWindowAttributes attrs;
+   attrs.override_redirect = true;
+
+   if (!XMatchVisualInfo(ctx.d, DefaultScreen(ctx.d), 32, TrueColor, &ctx.vinfo)) {
+      printf("No visual found supporting 32 bit color, terminating\n");
+      exit(EXIT_FAILURE);
+   }
+   // these next three lines add 32 bit depth, remove if you dont need and change the flags below
+   attrs.colormap = XCreateColormap(ctx.d, ctx.root, ctx.vinfo.visual, AllocNone);
+   attrs.background_pixel = 0;
+   attrs.border_pixel = 0;
+
+   // Window XCreateWindow(
+   //     Display *display, Window parent,
+   //     int x, int y, unsigned int width, unsigned int height, unsigned int border_width,
+   //     int depth, unsigned int class, 
+   //     Visual *visual,
+   //     unsigned long valuemask, XSetWindowAttributes *attributes
+   // );
+   ctx.overlay = XCreateWindow(
+      ctx.d, ctx.root,
+      0, 0, ctx.screen_w, ctx.screen_h, 0,
+      ctx.vinfo.depth, InputOutput, 
+      ctx.vinfo.visual,
+      CWOverrideRedirect | CWColormap | CWBackPixel | CWBorderPixel, &attrs
+   );
+
+   XserverRegion region = XFixesCreateRegion(ctx.d, 0, 0);
+   XFixesSetWindowShapeRegion(ctx.d, ctx.overlay, ShapeBounding, 0, 0, 0);
+   XFixesSetWindowShapeRegion(ctx.d, ctx.overlay, ShapeInput, 0, 0, region);
+   XFixesDestroyRegion(ctx.d, region);
+
+   XFixesHideCursor(ctx.d, ctx.overlay);
+
+   XMapWindow(ctx.d, ctx.overlay);
+}
+
+void initialize_xinput_capture(WindowContext &ctx) {
    /*
    * Set mask to receive XI_RawMotion events. Because it's raw,
    * XWarpPointer() events are not included, you can use XI_Motion
@@ -118,78 +146,54 @@ int main() {
    evmasks[0].deviceid = XIAllMasterDevices;
    evmasks[0].mask_len = sizeof(mask_bytes);
    evmasks[0].mask = mask_bytes;
-   XISelectEvents(d, root, evmasks, 1);
+   XISelectEvents(ctx.d, ctx.root, evmasks, 1);
+}
 
+CairoContext initialize_cairo(WindowContext &ctx) {
+   CairoContext cairoCtx;
+   cairoCtx.surf = cairo_xlib_surface_create(ctx.d, ctx.overlay,
+      ctx.vinfo.visual, ctx.screen_w, ctx.screen_h);
+   cairoCtx.cr = cairo_create(cairoCtx.surf);
+   return cairoCtx;
+}
 
-
-   XSetWindowAttributes attrs;
-   attrs.override_redirect = true;
-
-   XVisualInfo vinfo;
-   if (!XMatchVisualInfo(d, DefaultScreen(d), 32, TrueColor, &vinfo)) {
-      printf("No visual found supporting 32 bit color, terminating\n");
-      exit(EXIT_FAILURE);
-   }
-   // these next three lines add 32 bit depth, remove if you dont need and change the flags below
-   attrs.colormap = XCreateColormap(d, root, vinfo.visual, AllocNone);
-   attrs.background_pixel = 0;
-   attrs.border_pixel = 0;
-
-   // Window XCreateWindow(
-   //     Display *display, Window parent,
-   //     int x, int y, unsigned int width, unsigned int height, unsigned int border_width,
-   //     int depth, unsigned int class, 
-   //     Visual *visual,
-   //     unsigned long valuemask, XSetWindowAttributes *attributes
-   // );
-   Window overlay = XCreateWindow(
-      d, root,
-      0, 0, screen_w, screen_h, 0,
-      vinfo.depth, InputOutput, 
-      vinfo.visual,
-      CWOverrideRedirect | CWColormap | CWBackPixel | CWBorderPixel, &attrs
-   );
-
-   // XSelectInput(d, overlay, NoEventMask);
-
-   XserverRegion region = XFixesCreateRegion(d, 0, 0);
-   XFixesSetWindowShapeRegion(d, overlay, ShapeBounding, 0, 0, 0);
-   XFixesSetWindowShapeRegion(d, overlay, ShapeInput, 0, 0, region);
-   XFixesDestroyRegion(d, region);
-
-   XFixesHideCursor(d, overlay);
-
-   XMapWindow(d, overlay);
-
-   cairo_surface_t* surf = cairo_xlib_surface_create(d, overlay,
-                               vinfo.visual,
-                               screen_w, screen_h);
-   cairo_t* cr = cairo_create(surf);
-
-   unsigned int mask = 0;
-
+Coordinate getPointerCoords(WindowContext &ctx) {
    Window unused_w;
    int unused_i;
-   XEvent event;
+   Coordinate current;
+   XQueryPointer(ctx.d, ctx.root,
+      &unused_w, &unused_w,
+      &current.x, &current.y,
+      &unused_i, &unused_i,
+      (unsigned int *) &unused_i);
+   return current;
+}
 
-   std::vector<Coordinate> pointer_history(10);
+int main() {
+   WindowContext ctx = initialize_xlib();
+   initialize_window(ctx);
+   initialize_xinput_capture(ctx);
+   CairoContext cairoCtx = initialize_cairo(ctx);
+
+
+   double ptr_size = 7.0;
+   Color ptr_color = {1,0,0,.75};
+   int trail_length = 10;
+
+   std::vector<Coordinate> pointer_history(trail_length);
    int cooldown = 0;
    for (;;) {
       for (int i=0; i<pointer_history.size()-1; i++) {
          pointer_history[i] = pointer_history[i+1];
       }
-      Coordinate current;
-      XQueryPointer(d, root,
-         &unused_w, &unused_w,
-         &current.x, &current.y,
-         &unused_i, &unused_i,
-         &mask);
+      Coordinate current = getPointerCoords(ctx);
       pointer_history[pointer_history.size()-1] = current;
-      draw(cr, pointer_history);
-      XFlush(d);
+      draw(cairoCtx.cr, pointer_history, ptr_size, ptr_color);
+      XFlush(ctx.d);
 
       if (cooldown == 0){
-         XNextEvent(d, &event);
+         XEvent event;
+         XNextEvent(ctx.d, &event);
          cooldown = 5;
       } else {
          std::this_thread::sleep_for(std::chrono::milliseconds(10));
@@ -198,10 +202,10 @@ int main() {
       // TODO: exit event
    }
 
-   cairo_destroy(cr);
-   cairo_surface_destroy(surf);
+   cairo_destroy(cairoCtx.cr);
+   cairo_surface_destroy(cairoCtx.surf);
 
-   XUnmapWindow(d, overlay);
-   XCloseDisplay(d);
+   XUnmapWindow(ctx.d, ctx.overlay);
+   XCloseDisplay(ctx.d);
    return 0;
 }
